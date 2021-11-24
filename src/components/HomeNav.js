1e03e4ac45045c31be53fbc3245ef0dfa1d36644
Magda <magdagozdalik28@gmail.com>
2021-11-24 14:48:16 +0000

    Added homeNav structure and styling

--- src/components/HomeNav.js
+++ src/components/HomeNav.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 const HomeNav = () => {
     return (
-        <div>
-            Navigation
-        </div>
+        <section className="homeNav">
+            <div className="homeNav__upper">
+                <button className="homeNav__upper__log">
+                    Zaloguj
+                </button>
+                <button className="homeNav__upper__log">
+                    Zaloz Konto
+                </button>
+            </div>
+            <div className="homeNav__lower">
+                <ul>
+                    <li className="homeNav__tabs">Start</li>
+                    <li className="homeNav__tabs">O co chodzi?</li>
+                    <li className="homeNav__tabs">O nas</li>
+                    <li className="homeNav__tabs">Fundacja i organizacje</li>
+                    <li className="homeNav__tabs">Kontakt</li>
+                </ul>
+            </div>
+        </section>
     );
 };
 
