f3ef935223e4b166d4a6cd218ab6af575dca5b75
Magda <magdagozdalik28@gmail.com>
2021-11-25 10:15:37 +0000

    added HomeThreeColumns structure

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -3,7 +3,24 @@ import React from 'react';
 const HomeThreeColumns = () => {
     return (
         <section className="homeThreeColumns" id="threeColumns">
-            Three Columns yellow
+            <div className="homeThreeColumns__wrapper">
+                <div className="homeThreeColumns__column">
+                    <div className="homeThreeColumns__number">10</div>
+                    <h4 className="homeThreeColumns__title">ODDANYCH WORKÓW</h4>
+                    <p className="homeThreeColumns__text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                </div>
+                <div className="homeThreeColumns__column">
+                    <div className="homeThreeColumns__number">5</div>
+                    <h4 className="homeThreeColumns__title">WSPARTYCH ORGANIZACJI</h4>
+                    <p className="homeThreeColumns__text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                </div>
+                <div className="homeThreeColumns__column">
+                    <div className="homeThreeColumns__number">7</div>
+                    <h4 className="homeThreeColumns__title">ZORGANIZOWANY ZBIÓREK</h4>
+                    <p className="homeThreeColumns__text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                </div>
+            </div>
+
         </section>
     );
 };
