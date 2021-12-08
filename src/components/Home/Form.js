c81f287c3fb99d0bd9e358c1309d2d3d64289a98
Magda <magdagozdalik28@gmail.com>
2021-12-08 23:32:37 +0000

    #1h Strona główna - wysyłka formularza change of validation

--- src/components/Home/Form.js
+++ src/components/Home/Form.js
@@ -2,105 +2,87 @@ import React, {useState} from 'react';
 
 const Form = () => {
     const [name, setName] = useState("");
-    const [nameError, setNameError] =useState(false);
-    const [email, setEmail]=useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] =useState("");
+    const [nameError, setNameError] = useState(false);
     const [emailError, setEmailError] = useState(false);
-    const [message, setMessage]=useState("");
-    const [messageError, setMessageError]=useState(false);
-    const [isSubmitted, setIsSubmitted]= useState(false);
-    const [apiErrorMsg, setApiErrorMsg]=useState([]);
+    const [messageError, setMessageError] = useState(false);
+    const [submitted, setIsSubmitted] = useState(false);
 
-    const validate = (e)=>{
-        setEmail(e.target.value);
-        const regex = /^\S+@\S+\.\S+$/
-        if ( regex.test(email) === false){
-            setEmailError(true);
+    const handleSubmit =(e)=> {
+        e.preventDefault();
+        validateName(name)
+        validateEmail(email)
+        validateMessage(message)
+        if (nameError || emailError || messageError) {
+            return
         } else {
-            setEmail(e.target.value);
-            setEmailError(false);
+            const messageInfo = {
+                name,
+                email,
+                message,
+            }
+            fetch("https://fer-api.coderslab.pl/v1/portfolio/contact",{
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                method: "POST",
+                body:JSON.stringify(messageInfo)
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status === "success") {
+                        console.log(data.status);
+                        setIsSubmitted(true);
+                        setName("");
+                        setEmail("");
+                        setMessage("");
+                        // setNameError(false);
+                        // setEmailError(false);
+                        // setMessageError(false);
+                    } else {
+                        setIsSubmitted(false);
+                            console.log(data.status);
+                    }
+                })
+                .catch(err => console.log(err));
         }
-    };
+        }
 
-    const validateName=(e)=>{
-        setName(e.target.value)
-        let wordCount= 0;
-        wordCount = e.target.value.split(" ").length;
-        if ( wordCount > 1 || (!name) || (e.target.value.match("^[a-zA-Z ]*$") === null)){
+    const validateName = (name)=> {
+        if(!/[a-zA-Z]+$/.test(name) ||  /^\s+$/.test(name) ) {
             setNameError(true);
+            setName("");
         } else {
-            setName(e.target.value);
             setNameError(false);
         }
     }
-
-    const validateMessage =(e)=>{
-        setMessage(e.target.value);
-        let characterCount= 0;
-        characterCount = e.target.value.split("").length;
-        if ( characterCount <=120 || (!message)){
-            setMessageError(true);
+    const validateEmail = (email)=>{
+        const regex = /^\S+@\S+\.\S+$/
+        if (!regex.test(email) ){
+            setEmailError(true);
+            setEmail("");
         } else {
-            setMessageError(false);
+            setEmailError(false);
         }
-    }
-
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        if (nameError || emailError || messageError) {
-            return;
-        }
-        //sending info
-        const messageInfo = {
-            name,
-            email,
-            message,
-            status:"success"
-        }
-        return sendMessageInfo(messageInfo);
-    }
-
-    const sendMessageInfo =(messageInfo)=>{
-        fetch("https://fer-api.coderslab.pl/v1/portfolio/contact",{
-            headers: {
-                // "Authorization":
-                "Content-Type": "application/json",
-            },
-            method: "POST",
-            body:JSON.stringify(messageInfo)
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === "success") {
-                    console.log("success")
-                    setIsSubmitted(true);
-                    setName("");
-                    setEmail("");
-                    setMessage("");
-                    setEmailError(false);
-                    setNameError(false);
-                    setMessageError(false);
-                } else {
-                    console.log("error");
-                    let apiErrorMsg=[];
-                    apiErrorMsg.push(data.errors)
-                    setApiErrorMsg(apiErrorMsg);
-                }
-            })
-            .catch(err => console.log(err));
+    };
+    const validateMessage = (message) => {
+            let characterCount= 0;
+            characterCount = message.split("").length;
+            if ( characterCount <=120 || (!message)){
+                setMessageError(true);
+            } else {
+                setMessageError(false);
+            }
     }
 
     return (
-        <>
-
         <form
             className="form"
             autoComplete="off"
             onSubmit={handleSubmit}
             >
-            {isSubmitted && <div className="form__submitted">Wiadomosc zostala wyslana!<br/>Wkrotce sie skontaktujemy.</div>}
-            {/*{apiErrorMsg.length>0 && apiErrorMsg.map((err,index)=>{*/}
-            {/*   console.log(err.msg)}*/}
-            {/*)}*/}
+            {submitted && <div className="form__submitted">Wiadomosc zostala wyslana!<br/>Wkrotce sie skontaktujemy.</div>}
             <div className="form__info">
                 <div className="form__control">
                     <label htmlFor="name">Wpisz swoje imie</label>
@@ -111,11 +93,11 @@ const Form = () => {
                         placeholder="Your name"
                         autoComplete="off"
                         value={name}
-                        onChange={validateName}
+                        onChange={(e)=>setName(e.target.value)}
                         required
-                        className= {nameError && "form__errorBorder"}
+                        style={{borderBottom: nameError && "2px solid red"}}
                     />
-                    {nameError && <span className="form__error" id="nameError">Name must be one word </span>}
+                    {nameError && <span className="form__error">Name needs to be one word and letters only</span>}
                 </div>
 
                 <div className="form__control">
@@ -127,27 +109,30 @@ const Form = () => {
                         placeholder="abc@xyz.pl"
                         autoComplete="off"
                         value={email}
-                        onChange={(e)=> {validate(e)}}
-                        required/>
-                    {emailError && <span className="form__error" id="emailError">Please write valid email address</span>}
+                        onChange={(e)=>setEmail(e.target.value)}
+                        required
+                        style={{borderBottom: emailError && "2px solid red"}}
+                    />
+                    {emailError && <span className="form__error">Provide valid email address</span>}
                 </div>
             </div>
             <div className="form__control">
                 <label htmlFor="message">Wpisz wiadomosc</label>
                 <textarea rows="5"
                           placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                          value={message}
                           minLength="120"
+                          value={message}
+                          onChange={(e)=> setMessage(e.target.value)}
                           required
-                          onChange={(e)=> {validateMessage(e)}}/>
-                            {messageError && <span className="form__error" id="messageError">Message must be min 120 characters</span>}
+                          style={{borderBottom: messageError && "2px solid red"}}
+                />
+                {messageError && <span className="form__error">Message must be min 120 characters</span>}
+
             </div>
             <div className="form__submit">
                 <input type="submit" value="Send" className="btn  btn--small "/>
-
             </div>
         </form>
-    </>
     );
 };
 
