76fc9c6942c2fa7abdd63de237815c1689b57865
Magda <magdagozdalik28@gmail.com>
2022-01-13 21:42:25 +0000

    Update step two

--- src/components/DonationForm/StepTwo.js
+++ src/components/DonationForm/StepTwo.js
@@ -1,7 +1,18 @@
 import React from 'react';
+
 import DonationFormMessage from "./DonationFormMessage";
+import Select from "./Select";
 
 const StepTwo = () => {
+
+    const options = [
+        { value: 1, label: "1" },
+        { value: 2, label: "2" },
+        { value: 3, label: "3" },
+        { value: 4, label: "4" },
+        { value: 5, label: "5" },
+    ];
+
     return (
         <section className="donationForm__step">
             <DonationFormMessage>
@@ -11,6 +22,20 @@ const StepTwo = () => {
             <div className="donationForm__container">
                 <p>Krok 2/4</p>
                 <h2>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</h2>
+
+                <div className="form-group">
+                    <label className="select">
+                        <span>Liczba 60l worków:</span>
+                        <Select
+                            className="select-input"
+                            // placeholder={bags ? bags : "- wybierz -"}
+                            options={options}
+                            defaultValue="bags"
+                            // value={bags ? options[bags - 1] : false}
+                            // onSelectChange={(e) => setBags(e.value)}
+                        />
+                    </label>
+                </div>
             </div>
         </section>
 
